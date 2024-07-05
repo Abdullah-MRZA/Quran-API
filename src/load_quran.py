470c3606732df469f4297320efec11c14d252085
Abdullah Mirza <abdullahprograms@gmail.com>
2024-07-05 13:33:05 +0100

    quran loading completed

---
+++ src/load_quran.py
@@ -0,0 +1,69 @@
+import json
+from dataclasses import dataclass
+
+
+"""
+(Example output)
+In [3]: with open("quran_en.json") as f:
+   ...:     import json
+   ...:     a = f.read()
+   ...:     b = json.loads(a)
+   ...:     b = json.dumps(b, indent=4)
+   ...:     print(b[:1000])
+   ...: 
+[
+    {
+        "id": 1,
+        "name": "\u0627\u0644\u0641\u0627\u062a\u062d\u0629",
+        "transliteration": "Al-Fatihah",
+        "translation": "The Opener",
+        "type": "meccan",
+        "total_verses": 7,
+        "verses": [
+            {
+                "id": 1,
+                "text": "\u0628\u0650\u0633\u06e1\u0645\u0650 \u0671\u0644\u0644\u0651\u064e\u0647\u0650 \u0671\u0644\u0631\u0651\u064e\u062d\u06e1\u0645\u064e\u0670\u0646\u0650 \u0671\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650",
+                "translation": "In the name of Allah, the Entirely Merciful, the Especially Merciful"
+            },
+            {
+                "id": 2,
+                "text": "\u0671\u0644\u06e1\u062d\u064e\u0645\u06e1\u062f\u064f \u0644\u0650\u0644\u0651\u064e\u0647\u0650 \u0631\u064e\u0628\u0651\u0650 \u0671\u0644\u06e1\u0639\u064e\u0670\u0644\u064e\u0645\u0650\u064a\u0646\u064e",
+                "translation": "[All] praise is [due] to Allah, Lord of the worlds"
+"""
+
+
+@dataclass()
+class ayah_data:
+    text: str
+    translation: str
+
+
+@dataclass
+class surah_data:
+    surah_name: str
+    surah_name_arabic: str
+    revelation_place: str
+    total_verses: int
+    verses: list[ayah_data]
+
+
+def load_quran(filename: str) -> list[surah_data]:
+    quran_data = []
+
+    with open("quran.json") as f:
+        quran = json.loads(f.read())
+        for surah in quran:
+            quran_data.append(
+                surah_data(
+                    surah["transliteration"],
+                    surah["name"],
+                    surah["type"],
+                    surah["total_verses"],
+                    [
+                        ayah_data(ayah["text"], ayah["translation"])
+                        for ayah in surah["verses"]
+                    ],
+                )
+            )
+
+    print(quran_data)
---
+++ src/main_program.py
@@ -0,0 +1,26 @@
+# import arabic_reshaper
+from rich import print
+
+# from bidi.algorithm import get_display
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+import json
+
+
+# configuration
+# font_size = 36
+# width = 200
+# height = 100
+# back_ground_color = (255, 255, 255)
+# font_size = 36
+# font_color = (0, 0, 0)
+#
+# unicode_text = "بِسم اللَه"
+# # unicode_text_reshaped = arabic_reshaper.reshape(unicode_text)
+# # unicode_text_reshaped_RTL = get_display(unicode_text_reshaped, base_dir="R")
+#
+# im = Image.new("RGB", (width, height), back_ground_color)
+# draw = ImageDraw.Draw(im)
+# unicode_font = ImageFont.truetype("Al Majeed Quranic Font_shiped.ttf", font_size)
+# draw.text((10, 10), unicode_text, font=unicode_font, fill=font_color)
+#
+# im.save("text.png")
